9738bbf81aefdbdd438ed44ddb6bfbc5d42ab000
ChiragHitachi <chirag.gupta@hitachiconsulting.com>
2016-04-27 16:32:53 +0530

    Update HotelHelper.cs

--- HotelReview.Domain/HotelHelper.cs
+++ HotelReview.Domain/HotelHelper.cs
@@ -14,6 +14,7 @@ namespace TypeScriptDemo.Domain
               new Hotel(){ HotelId = 2, Name = "Hyatt", City = "Pune"},
               new Hotel(){ HotelId = 3, Name = "Barbossa", City = "Pune"},
               new Hotel(){ HotelId = 4, Name = "Orchid", City = "Pune"}
+               new Hotel(){ HotelId = 5, Name = "Hilton", City = "Pune"}
           };
 
         static IList<HotelReview> HotelReviews = new List<HotelReview> {
